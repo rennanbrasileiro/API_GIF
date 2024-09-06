70093ffd4559dc8cf87d9064d806de6b4bcf3bf5
rennanbrasileiro <rennanbrasileiro@gmail.com>
2024-09-05 23:23:11 -0300

    Ajuste na estrura do projeto e inclusão do banco H2

---
+++ src/main/java/com/example/solicitacaoapi/SolicitacaoAPI.java
@@ -0,0 +1,14 @@
+package com.example.solicitacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitacaoAPI {
+
+  public static void main(String[] args) {
+SpringApplication.run(SolicitacaoAPI.class, args);
+  }
+
+}
+
