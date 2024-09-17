7f07ec30bdb426305ac3755019d23a9f9dc79d39
rennanbrasileiro <rennanbrasileiro@gmail.com>
2024-09-17 16:25:05 -0300

    Mudanças no swagger

--- src/main/java/com/example/solicitacaoapi/config/SwaggerConfig.java
+++ src/main/java/com/example/solicitacaoapi/config/SwaggerConfig.java
@@ -1,32 +1,6 @@
 package com.example.solicitacaoapi.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.service.ApiInfo;
-
-@Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.solicitacaoapi.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Documentação da API de Solicitações")
-                .description("Esta é a documentação da API de Solicitações.")
-                .version("1.0.0")
-                .build();
-    }
 }
